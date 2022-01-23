8148de4378cbd8e2ed2f437a4530839ea4d55a09
Jqguqr <eduard0.estrada@hotmail.com>
2022-01-23 00:58:15 -0600

    Programs updated

--- 3_tipoOperadores.sh
+++ 3_tipoOperadores.sh
@@ -14,20 +14,23 @@ echo "Division A / B = " $((numA/numB))
 echo "Residuo A % B = " $((numA%numB))
 
 
-echo "\nOperadores Relacionales"
+echo -e  "\nOperadores Relacionales"
 echo "Numero A = $numA y Numero B = $numB"
 echo "Mayor que: A > B = " $((numA>numB))
 echo "Menor que: A < B = " $((numA<numB))
 echo "Mayor o igual que: A >= B = " $((numA>=numB))
-echo "Menor o igual que: A <= B = " $((numA>=numB))
+echo "Menor o igual que: A <= B = " $((numA<=numB))
 echo "Igual que: A == B = " $((numA==numB))
 echo "Diferente que: A != B = " $((num!=numB))
 
 
-echo "\nOperadores de Asignacion"
-echo "Suma A += B = " $((numA+=numB))
-echo "Resta A -= B = " $((numA-=numB))
-echo "Multiplicacion A *= B = " $((numA*=numB))
-echo "Division A /= B = " $((numA/=numB))
-echo "Residuo A %= B = " $((numA%=numB))
+echo -e  "\nOperadores de Asignacion"
+echo "Numero A = $numA y Numero B = $numB"
+echo "Suma A += B = " $((numA += numB))
+echo "Resta A -= B = " $((numA -= numB))
+echo "Multiplicacion A *= B = " $((numA *= numB))
+echo "Division A /= B = " $((numA /= numB))
+echo "Residuo A %= B = " $((numA %=  numB))
+
+
 
